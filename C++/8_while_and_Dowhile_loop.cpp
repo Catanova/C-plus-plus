f07899843d238fd80046849917a0b531c7622e4f
Awaleh Houssein <awaleh.houssein@g.austincc.edu>
2021-06-13 23:53:46 -0500

    while and do while

---
+++ C++/8_while_and_Dowhile_loop.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+cout << "---------------1st while Loop"<< endl;
+	int t=0;
+	while (t <4)
+	{
+		cout << "The city of Austin."<< endl;
+		
+		t++;
+	}
+	cout << "---------------2st while Loop"<< endl;
+	int f=0;
+	do 
+	{
+		cout << "Hello world."<< endl;
+		f++;
+	}
+	while (f <4);
+	
+
+}
