7a00ecc7eb6bd038c4e5d1496595a0b642671db4
Awaleh Houssein <awaleh.houssein@g.austincc.edu>
2021-06-22 19:32:09 -0500

    convert double and float to integer

---
+++ C++/15_convert_double_float_to_integer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double s1= 18.980976;
+	float s2= 6.98;
+	int x;
+	
+	x = (int) s1; // converting double to integer
+	cout << "s1: "<< x << endl;
+	x = (int) s2; // converting float to integer
+	cout << "s2: "<< x  << endl;
+	
+}
