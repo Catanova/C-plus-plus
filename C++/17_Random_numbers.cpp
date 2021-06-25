257d0933e044a0c69e4a3a09916e75c6359a9d73
Awaleh Houssein <awaleh.houssein@g.austincc.edu>
2021-06-24 21:10:39 -0500

    random

---
+++ C++/17_Random_numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+	
+	//generating random number each time
+	for (int x=1; x <10;x++){
+		cout <<"x"<<x << ":"<< rand()<< endl ;
+	}
+}
